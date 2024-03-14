dba3c192748a1e1110e21510f9a89ea494a102ac
Brandon Randle <133993158+cordialTurnip@users.noreply.github.com>
2024-03-14 08:52:20 -0500

    Vision to Ethernet Smartdashboard

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -87,11 +87,11 @@ public class Constants {
 	}
 
 	public static final class Vision {
-		public static final Transform3d shootCameraToRobot = new Transform3d(
-				new Translation3d(Units.inchesToMeters(8.8), Units.inchesToMeters(11.2), Units.inchesToMeters(6.8)),
-				new Rotation3d(Units.degreesToRadians(5), Units.degreesToRadians(65), 0));
+		// public static final Transform3d shootCameraToRobot = new Transform3d(
+		// 		new Translation3d(Units.inchesToMeters(8.8), Units.inchesToMeters(11.2), Units.inchesToMeters(6.8)),
+		// 		new Rotation3d(Units.degreesToRadians(5), Units.degreesToRadians(65), 0));
 
-		public static final Matrix<N3, N1> kDefaultStdDevs = VecBuilder.fill(0.2, 0.2, 0.2);
+		// public static final Matrix<N3, N1> kDefaultStdDevs = VecBuilder.fill(0.2, 0.2, 0.2);
 
 		// public static final double X_P = 2.7;
 		// public static final double X_I = 0.0;
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,34 +6,23 @@ package frc.robot;
 
 import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
-import com.pathplanner.lib.pathfinding.LocalADStar;
-import com.pathplanner.lib.pathfinding.Pathfinding;
 
-import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import frc.robot.commands.ActuateIntake;
-import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Elevator.ElevatorState;
 import frc.robot.subsystems.Indexer.IndexerState;
 import frc.robot.subsystems.Intake.IntakeState;
 // import frc.robot.subsystems.Orchestrator.OrchestratorState;
 import frc.robot.subsystems.Shooter.ShooterState;
-import frc.robot.subsystems.Vision;
 import frc.robot.subsystems.Wrist.WristState;
-import frc.util.InterpolatingDouble;
 
 public class Robot extends TimedRobot {
   private Command autoCommand;
@@ -55,8 +44,8 @@ Pose2d speakerPose = new Pose2d(15.3, 5.50, Rotation2d.fromDegrees(180));
     robotContainer = new RobotContainer();
     RobotContainer.drivetrain.seedFieldRelative(speakerPose);
 
-    // PortForwarder.add(1181, "photonvision.local", 1182);
-    // PortForwarder.add(1183, "photonvision.local", 1184);
+    PortForwarder.add(1181, "photonvision.local", 1182);
+    PortForwarder.add(1183, "photonvision.local", 1184);
     RobotContainer.drivetrain.getDaqThread().setThreadPriority(99);
     SmartDashboard.putData("Auto Selector", autoChooser);
     for (int module = 0; module < 3; module++) {
